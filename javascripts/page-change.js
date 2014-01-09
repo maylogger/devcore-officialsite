ff0d641f495ff308ca666ab6a3c8d31eca34f396
shengli1989 <shengli@evendesign.tw>
2014-01-09 21:40:57 +0800

    page-change

---
+++ javascripts/page-change.js
@@ -0,0 +1,13 @@
+var pageChange = angular.module("pageChange", ["ngAnimate", "ngRoute"], function($routeProvider, $locationProvider){
+  $routeProvider.when('/', {
+    templateUrl: '/templates/home.html'
+  }).when('/about', {
+    templateUrl: '/templates/about.html'
+  }).when('/shop', {
+    templateUrl: '/templates/shop.html'
+  }).when('/portfolio', {
+    templateUrl: '/templates/portfolio.html'
+  }).when('/contact', {
+    templateUrl: '/templates/contact.html'
+  });
+});

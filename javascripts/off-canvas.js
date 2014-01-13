7a4a9b8e7db84f86a4000cbf653d0fd85fed0bc0
shengli1989 <shengli@evendesign.tw>
2014-01-13 20:24:49 +0800

    修改 off-canvas.js

--- javascripts/off-canvas.js
+++ javascripts/off-canvas.js
@@ -1,24 +1,12 @@
 var off_canvas = function (){
+
   $(".off-canvas-toggle-btn").on("click", function(){
     $("body").toggleClass("open-menu");
   });
 
-  $(".off-canvas-close-btn").on("click", function(){
-    removeOffCanvas();
-  });
-
-  $(".off-canvas-mask").on("click", function(){
-    removeOffCanvas();
-  });
-
-  // 點 menu 裡面的連結後，關閉 menu
-  $(".off-canvas-menu a").on("click", function(){
-    removeOffCanvas();
-  });
-
-  function removeOffCanvas(){
+  $(".outside a, .off-canvas-mask").on("click", function(){
     $("body").removeClass("open-menu");
-  }
+  });
 
 };
 

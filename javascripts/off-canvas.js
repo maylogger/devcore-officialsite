0b67b7594f61a7b35e4d505decdd4142d4894732
shengli1989 <shengli@evendesign.tw>
2014-01-16 16:52:29 +0800

    prevent off-canvas-btn bug

--- javascripts/off-canvas.js
+++ javascripts/off-canvas.js
@@ -1,7 +1,8 @@
 var off_canvas = function (){
 
-  $(".off-canvas-toggle-btn").on("click", function(){
+  $(".off-canvas-toggle-btn").on("click", function(event){
     $("body").toggleClass("open-menu");
+    event.preventDefault;
   });
 
   $(".outside a, .off-canvas-mask").on("click", function(){

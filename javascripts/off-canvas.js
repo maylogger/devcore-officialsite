626b293f2409c9a7b14c81dce3d398aa6d632d3b
Tou Neko <touneko@gmail.com>
2014-01-29 00:36:23 +0800

    加入 model class 做二階段效果

--- javascripts/off-canvas.js
+++ javascripts/off-canvas.js
@@ -1,12 +1,15 @@
 var off_canvas = function (){
 
   $(".off-canvas-toggle-btn").on("click", function(event){
-    $("body").toggleClass("open-menu");
+    $("body").toggleClass("model open-menu");
     event.preventDefault;
   });
 
   $(".outside a, .off-canvas-mask, .outside .close").on("click", function(){
-    $("body").removeClass("open-menu");
+    $("body").removeClass("open-menu").delay(500).queue(function(){
+			$(this).removeClass("model");
+			$(this).dequeue();
+    });
   });
 
 };

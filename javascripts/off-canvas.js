f6ff1e4a8eb323fbe84b41535dd80abc72e734c9
evenwu <evendesign@gmail.com>
2014-01-30 05:04:18 +0800

    off canvas done

--- javascripts/off-canvas.js
+++ javascripts/off-canvas.js
@@ -1,15 +1,20 @@
 var off_canvas = function (){
 
-  $(".off-canvas-toggle-btn").on("click", function(event){
-    $("body").toggleClass("model open-menu");
-    event.preventDefault;
+  var menuStatus = $("body");
+  var openMenuButton = $(".open-menu-btn");
+  var closeMenuButton = $(".outside .close-menu-btn");
+
+  openMenuButton.on("click", function(event){
+    menuStatus.addClass("model open-menu");
+    event.preventDefault();
   });
 
-  $(".outside a, .off-canvas-mask, .outside .close").on("click", function(){
-    $("body").removeClass("open-menu").delay(500).queue(function(){
-			$(this).removeClass("model");
-			$(this).dequeue();
+  closeMenuButton.on("click", function(){
+    menuStatus.removeClass("open-menu").delay(500).queue(function(){
+			menuStatus.removeClass("model");
+			menuStatus.dequeue();
     });
+    event.preventDefault();
   });
 
 };

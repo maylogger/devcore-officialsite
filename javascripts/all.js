728339f0a246c27c55929456dddcdcbf7432efe1
shengli1989 <shengli@evendesign.tw>
2014-02-14 17:41:44 +0800

    調整服務系列頁面 page-header 與 services-nav

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,2 +1,9 @@
 // 移動 dom
 // $( ".main-nav ul" ).appendAround();
+
+// 讓服務頁副導覽，可以在第一畫面中顯示現在頁面標示。
+if ( $('.services-nav').length != 0 ) {
+  var services_nav_scroll_bar = $('.services-nav');
+  var services_nav_item_active_offset_left = $('.services-nav-item.active').offset().left;
+  services_nav_scroll_bar.scrollLeft(services_nav_item_active_offset_left);
+}

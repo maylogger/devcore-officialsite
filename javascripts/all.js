ef9d14462eceb856ed1bce995b50dac3c231bdff
shengli1989 <shengli@shenglideMacBook-Pro.local>
2014-02-16 16:40:31 +0800

    簡化 scrollbar js code

--- javascripts/all.js
+++ javascripts/all.js
@@ -3,7 +3,5 @@
 
 // 讓服務頁副導覽，可以在第一畫面中顯示現在頁面標示。
 if ( $('.services-nav').length != 0 ) {
-  var services_nav_scroll_bar = $('.services-nav');
-  var services_nav_item_active_offset_left = $('.services-nav-item.active').offset().left;
-  services_nav_scroll_bar.scrollLeft(services_nav_item_active_offset_left);
+  $('.services-nav').scrollLeft( $('.services-nav-item.active').offset().left );
 }
